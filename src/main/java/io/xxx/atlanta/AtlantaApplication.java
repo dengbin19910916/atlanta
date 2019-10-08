8314f19f650dc511603262561bb0e0e84137b494
Dengbin <dengbin@winnermedical.com>
2019-10-08 21:28:07 +0800

    Initial commit

---
+++ src/main/java/io/xxx/atlanta/AtlantaApplication.java
@@ -0,0 +1,13 @@
+package io.xxx.atlanta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlantaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtlantaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/xxx/atlanta/AtlantaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.xxx.atlanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlantaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
